90d7c3e0c7e8fee54d30e96af667e0d77b79b1fa
NikeshRaj-000 <129160627+NikeshRaj-000@users.noreply.github.com>
2023-03-28 15:27:14 +0530

    Car Src

---
+++ Car.java
@@ -0,0 +1,13 @@
+package com.supreme;
+
+public class Car {
+public void name() {
+	System.out.println("MRD");
+
+}
+public static void main(String[] args) {
+	Car ar = new Car();
+	ar.name();
+	
+}
+}
